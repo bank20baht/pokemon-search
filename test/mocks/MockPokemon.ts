bd0f8e9925cecbdff37b58f51cf7981172667169
Nattapong Promthong <banker.z412@gmail.com>
2024-03-31 14:40:49 +0700

    feat: add jest as test

---
+++ test/mocks/MockPokemon.ts
@@ -0,0 +1,26 @@
+export const MockBulbasaur = {
+  pokemon: {
+    name: "Bulbasaur",
+    id: "UG9rZW1vbjowMDE=",
+    image: "https://img.pokemondb.net/artwork/bulbasaur.jpg",
+    types: ["Grass", "Poison"],
+  },
+};
+
+export const MockCharmander = {
+  pokemon: {
+    name: "Charmander",
+    id: "UG9rZW1vbjowMDQ=",
+    image: "https://img.pokemondb.net/artwork/charmander.jpg",
+    types: ["Fire"],
+  },
+};
+
+export const MockSquirtle = {
+  pokemon: {
+    name: "Squirtle",
+    id: "UG9rZW1vbjowMDc=",
+    image: "https://img.pokemondb.net/artwork/squirtle.jpg",
+    types: ["Water"],
+  },
+};
